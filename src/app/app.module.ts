a3e1a6d3ad0f9f35f482e253a31f0c47e2fb34bc
Zolboo <zolboo.deb@gmail.com>
2017-07-28 10:44:46 +0200

    weather

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { TypeaheadModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
+import { DayComponent } from './day.component';
+import { DayExpandedComponent } from './day-expanded.component';
+import { ForecastService } from './forecast.service'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DayComponent,
+    DayExpandedComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    TypeaheadModule.forRoot()
   ],
-  providers: [],
+  providers: [ForecastService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
